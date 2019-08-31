89a4d749d7a0dee4345f159bd72d4bc4c5e32152
Geoff Hom <geoffhom@gmail.com>
2019-08-31 01:49:20 -0700

    Version 1.0 (build 3). Show version number.

---
+++ HanabiAi/Models/AppInfo.swift
@@ -0,0 +1,22 @@
+//
+//  AppInfo.swift
+//  HanabiAi
+//
+//  Abstract:
+//
+//  Created by Geoff Hom on 8/30/19.
+//  Copyright © 2019 Geoff Hom. All rights reserved.
+//
+
+import Foundation
+
+struct AppInfo {
+    private static let mainBundle = Bundle.main
+    
+    private static let versionKey = "CFBundleShortVersionString"
+    // There's no constant for "CFBundleShortVersionString."
+    static let version = mainBundle.object(forInfoDictionaryKey: versionKey) as! String
+    
+    private static let buildKey = kCFBundleVersionKey as String
+    static let build = mainBundle.object(forInfoDictionaryKey: buildKey) as! String
+}
