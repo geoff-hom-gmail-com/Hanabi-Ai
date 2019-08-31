e56e0ec89955964dd99a434e61576da2e5178c70
Geoff Hom <geoffhom@gmail.com>
2019-08-31 16:20:50 -0700

    Version 1.0 (build 7). Populate "About" screen.

--- Hanabi Ai/AboutView.swift
+++ Hanabi Ai/AboutView.swift
@@ -10,11 +10,30 @@ import SwiftUI
 
 struct AboutView: View {
     var body: some View {
-        VStack {
-            Text("This is the About screen!")
-//            Spacer()
-            Divider()
-            Text("Version \(AppInfo.version) (build \(AppInfo.build))")
+        // Using Form until Text.lineLimit(nil) starts working robustly.
+        Form {
+            Section {
+                Text("Hanabi Ai is a simulator for the card game Hanabi.")
+            }
+            
+            VStack(alignment: .leading) {
+                Text("Use Hanabi Ai to:")
+                Text("• Auto-play many games in a row.")
+                Text("• Auto-play a specific game (i.e., deck setup).")
+            }
+            
+            VStack(alignment: .leading) {
+                Text("Notes:")
+                Text("• Only 2-player games for now.")
+                Text("• Only 1 AI personality offered:")
+                Text("      • Superman: Has X-ray vision, so knows all cards, including the deck.")
+                Text("• % wins: TBD (1 in X)")
+                Text("• Losing-deck example: TBD")
+            }
+            
+            Section {
+                Text("Version \(AppInfo.version) (build \(AppInfo.build))")
+            }
         }
         .navigationBarTitle(Text("About"))
     }
